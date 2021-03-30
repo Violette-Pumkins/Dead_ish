f4bc7f940251e30ad715a2149491409625ef3d12
Violette Pumkins <Violette-Pumkins@github.com>
2021-03-30 15:21:13 +0200

    first commit

---
+++ state.js
@@ -0,0 +1,12 @@
+
+$state=0;
+if ($state=0) {
+    document.getElementById("text");
+    text.innerHTML = "U R DEAD!"
+}
+else if ($state=1) {
+    console.log("U R ALIVE!");
+}
+else if ($state=2) {
+    console.log("U R a ZOMBIE!");
+}
